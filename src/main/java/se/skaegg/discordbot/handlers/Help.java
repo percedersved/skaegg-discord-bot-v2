7fbb72977f08d681a726f485960f256c20d32d8f
percedersved <per.cedersved@gmail.com>
2022-07-11 23:25:36 +0200

    Added table for statistics where all command calls are saved. Also added /statistik command to show this info

--- src/main/java/se/skaegg/discordbot/handlers/Help.java
+++ src/main/java/se/skaegg/discordbot/handlers/Help.java
@@ -30,7 +30,7 @@ public class Help implements SlashCommand{
                 "`/nedräkning lista` - Listar nedräkningar med namn och ID\n" +
                 "`/nedräkning tabort [ID]` - Tar bort nedräkning\n" +
                 "`/restaurang` - Tips på restaurang. Default är i Norrtälje. Möjligt att lägga till annan stad eller plats som parameter\n" +
-                "`/restaurangduell` - Rösta på 2 olika slumpmässiga restauranger. Default är i Norrtälje. Möjligt att lägga till annan stad eller plats som parameter" +
+                "`/restaurangduell` - Rösta på 2 olika slumpmässiga restauranger. Default är i Norrtälje. Möjligt att lägga till annan stad eller plats som parameter\n" +
                 "`/statistik` - Visar statistik för kommandon och vilka användare som använt boten mest";
 
         EmbedCreateSpec embed = EmbedCreateSpec.builder()
