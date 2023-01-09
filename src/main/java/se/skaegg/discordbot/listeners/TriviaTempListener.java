af17b4d2a4705c490f6c6cc8a86f42e94f244fa7
percedersved <per.cedersved@gmail.com>
2023-01-09 22:24:20 +0100

    Fixed issue where multiple listeners where catching the same button presses if you answered multiple question in a short while

--- src/main/java/se/skaegg/discordbot/listeners/TriviaTempListener.java
+++ src/main/java/se/skaegg/discordbot/listeners/TriviaTempListener.java
@@ -7,6 +7,7 @@ import se.skaegg.discordbot.handlers.Trivia;
 
 import java.time.Duration;
 import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class TriviaTempListener {
 
@@ -14,13 +15,16 @@ public class TriviaTempListener {
 
     public Mono<Void> createTempListener(Trivia trivia, GatewayDiscordClient client) {
 
+        AtomicBoolean listenerUsed = new AtomicBoolean(false);
         client.on(ButtonInteractionEvent.class, buttonEvent -> {
-                    if (buttonEvent.getCustomId().contains("trivia_")) {
+                    if (buttonEvent.getCustomId().contains("trivia_") && !listenerUsed.get()) {
+                        listenerUsed.set(true); // Need to set this to know if the listener has been used. If you answer multiple questions in a short time mutliple listeners will listen otherwise.
                         return trivia.checkAnswer(buttonEvent);
                     }
                     else {
                         return Mono.empty();
                     }
+
                 })
                 .timeout(Duration.ofMinutes(30L))
                 .onErrorResume(TimeoutException.class, ignore -> Mono.empty())
