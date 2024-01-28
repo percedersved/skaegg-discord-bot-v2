9d702a8340cf230092328182bf9a5f2a62def4c7
Per Cedersved <per.cedersved@easit.com>
2024-01-28 12:45:15 +0100

    Added restcontroller to get stats

--- src/main/java/se/skaegg/discordbot/jpa/EmojiStatsRepository.java
+++ src/main/java/se/skaegg/discordbot/jpa/EmojiStatsRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -12,4 +13,14 @@ public interface EmojiStatsRepository extends JpaRepository<EmojiStatsEntity, In
     @Query("SELECT new se.skaegg.discordbot.jpa.EmojiStatsCount(e.name, COUNT(e.id)) "
             + "FROM EmojiStatsEntity AS e GROUP BY e.name ORDER BY COUNT(e.id) DESC")
     List<EmojiStatsCount> countTotalPerEmoji();
+
+    List<EmojiStatsEntity> findAllByDateBetween(LocalDate from, LocalDate to);
+
+//    @Query("SELECT new se.skaegg.discordbot.jpa.EmojiStatsCountPerDay(date, name, COUNT(*)) "
+//            + "FROM EmojiStatsEntity WHERE date BETWEEN ?1 AND ?2 GROUP BY date, name ORDER BY date")
+//    List<EmojiStatsCountPerDay> countTotalPerDayDateFilter(LocalDate from, LocalDate to);
+
+    @Query("SELECT new se.skaegg.discordbot.jpa.EmojiStatsCountPerDay(e.date, e.name, e.useType, COUNT(*)) "
+            + "FROM EmojiStatsEntity AS e GROUP BY e.date, e.name, e.useType ORDER BY e.date")
+    List<EmojiStatsCountPerDay> countTotalPerDay();
 }
