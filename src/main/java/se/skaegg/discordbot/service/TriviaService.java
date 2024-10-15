8d945ea86d1bf6ffebee6264d12f6e3e1eda97fb
Per Cedersved <per.cedersved@easit.com>
2024-10-15 21:56:40 +0200

    New endpoint for fetching a single trivia question based on date

--- src/main/java/se/skaegg/discordbot/service/TriviaService.java
+++ src/main/java/se/skaegg/discordbot/service/TriviaService.java
@@ -1,11 +1,14 @@
 package se.skaegg.discordbot.service;
 
-import se.skaegg.discordbot.dto.TriviaScorersByDate;
-
 import java.time.LocalDate;
 import java.util.List;
+
+import se.skaegg.discordbot.dto.TriviaQuestionDTO;
+import se.skaegg.discordbot.dto.TriviaScorersByDate;
 
 public interface TriviaService {
 
     List<TriviaScorersByDate> getTriviaScorersPerDay(LocalDate from, LocalDate to);
+
+	TriviaQuestionDTO getTriviaQuestion(LocalDate date);
 }
