a79d4bf92ee6c93fa963454d126a18b3f88d572d
percedersved <per.cedersved@gmail.com>
2024-10-15 22:05:28 +0200

    New endpoint for fetching a single trivia question based on date (#14)
    Co-authored-by: Per Cedersved <per.cedersved@easit.com>

--- src/main/java/se/skaegg/discordbot/service/TriviaServiceImpl.java
+++ src/main/java/se/skaegg/discordbot/service/TriviaServiceImpl.java
@@ -1,16 +1,19 @@
 package se.skaegg.discordbot.service;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import se.skaegg.discordbot.dto.TriviaQuestionDTO;
 import se.skaegg.discordbot.dto.TriviaScorersByDate;
+import se.skaegg.discordbot.entity.TriviaQuestions;
 import se.skaegg.discordbot.repository.TriviaQuestionsRepository;
 import se.skaegg.discordbot.repository.TriviaScoresRepository;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Objects;
 
 @Service
 public class TriviaServiceImpl implements TriviaService {
@@ -48,5 +51,18 @@ public class TriviaServiceImpl implements TriviaService {
                 })
                 .filter(Objects::nonNull) // Remove nulls
                 .toList();
+    }
+
+    @Override
+    public TriviaQuestionDTO getTriviaQuestion(LocalDate date) {
+        TriviaQuestions question = triviaQuestionsRepository.findByQuestionDate(date);
+        return new TriviaQuestionDTO(question.getId(),
+                question.getQuestion(),
+                question.getQuestionDate(),
+                question.getCorrectAnswer(),
+                question.getIncorrectAnswers(),
+                question.getCategory(),
+                question.getDifficulty(),
+                question.getType());
     }
 }
